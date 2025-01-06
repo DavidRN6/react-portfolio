3bc18de9d5570fb905e4d5e91238873ef1667817
David <yugiohgxd600@gmail.com>
2025-01-06 13:16:51 -0800

    contact finished

--- src/Components/4-Contact/Contact.jsx
+++ src/Components/4-Contact/Contact.jsx
@@ -1,9 +1,39 @@
-import './Contact.css'
+import "./Contact.css";
+import { MdEmail } from "react-icons/md";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <section className="contact">
+      <h1 className="title">
+        <span className="email-icon">
+          <MdEmail />
+        </span>{" "}
+        Contact us
+      </h1>
+      <p>
+        Contact us for more information and get notified when I pushlish
+        something new.
+      </p>
+
+      <div className="field">
+        <form>
+          <div>
+            <label htmlFor="email">Email Address:</label>
+            <input type="email" id="email" />
+          </div>
+
+          <div className="textarea">
+            <label htmlFor="message">Your Message:</label>
+            <textarea required id="message"></textarea>
+          </div>
+
+          <button className="submit">Submit</button>
+        </form>
+
+        <div className="border animation">animation</div>
+      </div>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
